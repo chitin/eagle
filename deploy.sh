aec41ba5abdac5b6b7aa2a35fc9d44a54c3ce4b4
Lingang Deng <chitin1027@gmail.com>
2017-05-11 18:08:32 +0800

    Update deploy.sh
    rename master to beam

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "${TRAVIS_EVENT_TYPE}"
 
 git clone https://github.com/1haodian/eagle.git eagle
 cd eagle
-git checkout master
+git checkout beam
 git status
 
 
@@ -25,7 +25,7 @@ echo "Rebase..."
 git rebase upstream/master 
 
 echo "Pushing with force ..."
-git push --force origin master > /dev/null 2>&1 || exit 1
+git push --force origin beam > /dev/null 2>&1 || exit 1
 echo "Pushed deployment successfully"
 
 exit 0
